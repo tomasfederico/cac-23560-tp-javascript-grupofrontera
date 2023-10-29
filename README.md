1aa115c2399d432b9d911b9dfa84d4104086b927
Tomas <47828236+tomasfederico@users.noreply.github.com>
2023-10-29 00:11:07 -0300

    Primer Commit - Creacion de Repositorio
    Incluye el front end y funciones de javascript para poder realizar las funcionalidades del formulario.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Codo a Codo
+# Trabajo Practico Grupal JavaScript
+## COMISION 23560 - Grupo Frontera
+## Integrantes:
+### Braulio
+### Tomas
+### Pedro
+### Gaston
+### Ernesto
+
+## ¿De que trata el proyecto?
+Simulación de formulario de compra de tickets para evento en Ciudad de Buenos Aires.
+
+## ¿Cual es la funcion del formulario?
+El formulario permite cargar los datos del comprador y generar una factura en base a la cantidad de tickets deseados según 3 categorias: Estudiante, Trainee y Junior. Cada categoría aplica a un descuento diferente.
+Existe una restricción de compra de 5 tickets como máximo.
+El usuario puede observar el calculo automático del valor final de la factura en base a los tipos y cantidad de tickets seleccionados.
+Luego, al confirmarse la compra se emite un pop-up con la "factura" y detalle de la compra.-
---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+// CONSTANTES PARA EL CALCULO DE TICKETS
+const TICKET_VALUE = 200;
+const STUDENT_DISCOUNT = 0.80;
+const TRAINEE_DISCOUNT = 0.50;
+const JUNIOR_DISCOUNT = 0.15;
+
+// TRAER DATOS DEL HTML
+let ticketsStudents = document.getElementById("tickets-students");
+let ticketsTrainees = document.getElementById("tickets-trainees"); 
+let ticketsJuniors = document.getElementById("tickets-juniors"); 
+// EVENTLISTENERS PARA OBSERVAR VALORES DE LOS SELECTORES DE TKTS AL CAMBIARLOS
+ticketsStudents.addEventListener("change", function() {
+    console.log("tkts students: " + ticketsStudents.value);
+    tickets_total_count();
+});
+ticketsTrainees.addEventListener("change", function() {
+    console.log("tkts trainees: " + ticketsTrainees.value);
+    tickets_total_count();
+});
+ticketsJuniors.addEventListener("change", function() {
+    console.log("tkts juniors: " + ticketsJuniors.value);
+    tickets_total_count();
+});
+
+function tickets_total_count(){
+//FUNCION DE PRUEBA PARA CONTAR LOS TICKETS DE CADA ITEM Y MOSTRARLOS EN PANTALLA
+//RECIBE LA CANTIDAD DE TICKETS DE CADA CATEGORIA
+//CONTROLA SI LA SUMA DE LOS TICKETS SUPERA 5 UNIDADES
+//RETORNA VALOR TOTAL DE TODOS LOS TICKETS 
+//LA CUENTA ES LA SUMATORIA DE: 
+//VALOR TICKET TOTAL * (1-DESCUENTO DE ESA CATEGORIA) * CANTIDAD DE TICKETS DE ESA CATEGORIA
+//(TICKET_VALUE*(1-STUDENT_DISCOUNT)*ticketsStudents) + (TICKET_VALUE*(1-TRAINEE_DISCOUNT)*ticketsTrainees) + (TICKET_VALUE*(1-JUNIOR_DISCOUNT)*ticketsJuniors)
+
+    let ticketsStudents = parseInt(document.getElementById("tickets-students").value);
+    let ticketsTrainees = +document.getElementById("tickets-trainees").value; 
+    let ticketsJuniors = +document.getElementById("tickets-juniors").value; 
+
+    console.log(ticketsStudents);
+    console.log(ticketsTrainees);
+    console.log(ticketsJuniors);
+
+    let studentsTotal = TICKET_VALUE*(1-STUDENT_DISCOUNT)*ticketsStudents;
+    let traineesTotal = TICKET_VALUE*(1-TRAINEE_DISCOUNT)*ticketsTrainees;
+    let juniorsTotal = TICKET_VALUE*(1-JUNIOR_DISCOUNT)*ticketsJuniors;
+    let total = studentsTotal+traineesTotal+juniorsTotal;
+    
+    console.log("resultado final: " + total);
+    document.getElementById("total-cost-tickets").innerHTML = "Total a pagar: $" + total;
+}
+
+function reset_form(){
+    //RESETEA EL FORMULARIO
+    //MUESTRA UN 0 EN PANTALLA EN EL CALCULO DE TICKETS (HABRIA QUE REHACERLO X AUTOMATISMO)
+    //OCULTA EL MENSAJE DE ERROR CON .hidden
+    document.getElementById("form-ticket-purchase").reset();
+    document.getElementById("total-cost-tickets").innerHTML = "Total a pagar: $" + 0;
+    document.getElementById("error-message").hidden = true;
+}
+
