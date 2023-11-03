f25cfbf88fa66a910abd6afa0ac91b5871d20fd4
EMonfort <emonfort@fi.uba.ar>
2023-11-03 19:18:00 -0300

    Separa código para evitar repetición

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,32 +9,33 @@ let ticketsStudents = document.getElementById("tickets-students");
 let ticketsTrainees = document.getElementById("tickets-trainees");
 let ticketsJuniors = document.getElementById("tickets-juniors");
 let ticketsGenerals = document.getElementById("tickets-generals");
+
+let ticketsStudentsNumber = 0;
+let ticketsTraineesNumber = 0;
+let ticketsJuniorsNumber = 0;
+let ticketsGeneralsNumber = 0;
+
+
 // EVENTLISTENERS PARA OBSERVAR VALORES DE LOS SELECTORES DE TKTS AL CAMBIARLOS
 ticketsStudents.addEventListener("change", function () {
     console.log("tkts students: " + ticketsStudents.value);
-    tickets_total_count();
+    show_total_price();
 });
 ticketsTrainees.addEventListener("change", function () {
     console.log("tkts trainees: " + ticketsTrainees.value);
-    tickets_total_count();
+    show_total_price();
 });
 ticketsJuniors.addEventListener("change", function () {
     console.log("tkts juniors: " + ticketsJuniors.value);
-    tickets_total_count();
+    show_total_price();
 });
 ticketsGenerals.addEventListener("change", function () {
     console.log("tkts generals: " + ticketsJuniors.value);
-    tickets_total_count();
+    show_total_price();
 });
 
 function tickets_total_count() {
-    //FUNCION DE PRUEBA PARA CONTAR LOS TICKETS DE CADA ITEM Y MOSTRARLOS EN PANTALLA
-    //RECIBE LA CANTIDAD DE TICKETS DE CADA CATEGORIA
-    //CONTROLA SI LA SUMA DE LOS TICKETS SUPERA 5 UNIDADES
-    //RETORNA VALOR TOTAL DE TODOS LOS TICKETS 
-    //LA CUENTA ES LA SUMATORIA DE: 
-    //VALOR TICKET TOTAL * (1-DESCUENTO DE ESA CATEGORIA) * CANTIDAD DE TICKETS DE ESA CATEGORIA
-    //(TICKET_VALUE*(1-STUDENT_DISCOUNT)*ticketsStudents) + (TICKET_VALUE*(1-TRAINEE_DISCOUNT)*ticketsTrainees) + (TICKET_VALUE*(1-JUNIOR_DISCOUNT)*ticketsJuniors)
+    
 
     //Numero de PV
     const numramdom = [1596, 2586, 6574, 4586, 9658, 1452, 3652];
@@ -82,46 +83,7 @@ function tickets_total_count() {
     let PrecioUnitario = document.getElementById("PrecioUnitario");
     PrecioUnitario.textContent = "$" + TICKET_VALUE;
 
-    let ticketsStudents = parseInt(document.getElementById("tickets-students").value);
-    let ticketsTrainees = +document.getElementById("tickets-trainees").value;
-    let ticketsJuniors = +document.getElementById("tickets-juniors").value;
-    let ticketsGenerals = +document.getElementById("tickets-generals").value;
-
-    //cantidad de tk
-
-
-    //Precio Total
     
-
-    console.log(ticketsStudents);
-    console.log(ticketsTrainees);
-    console.log(ticketsJuniors);
-    console.log(ticketsGenerals);
-
-    let total
-
-
-
-
-    if (valid_total_ticket_number()) {
-        let studentsTotal = TICKET_VALUE * (1 - STUDENT_DISCOUNT) * ticketsStudents;
-        let traineesTotal = TICKET_VALUE * (1 - TRAINEE_DISCOUNT) * ticketsTrainees;
-        let juniorsTotal = TICKET_VALUE * (1 - JUNIOR_DISCOUNT) * ticketsJuniors;
-        let generalsTotal = TICKET_VALUE * ticketsGenerals;
-        total = (studentsTotal + traineesTotal + juniorsTotal + generalsTotal).toFixed(2);
-
-        document.getElementById("error-message").innerHTML = "";
-        document.getElementById("error-message").hidden = true;
-
-    } else {
-        total = "-"
-
-        document.getElementById("error-message").innerHTML = "Error: La cantidad total de tickets no puede ser superior a 5";
-        document.getElementById("error-message").hidden = false;
-    }
-    console.log("resultado final: " + total);
-
-    document.getElementById("total-cost-tickets").innerHTML = "Total a pagar: $" + total;
 }
 
 function reset_form() {
@@ -133,18 +95,45 @@ function reset_form() {
     document.getElementById("error-message").hidden = true;
 }
 
+function show_total_price() {
+    //FUNCION DE PRUEBA PARA CONTAR LOS TICKETS DE CADA ITEM Y MOSTRARLOS EN PANTALLA
+    //RECIBE LA CANTIDAD DE TICKETS DE CADA CATEGORIA
+    //CONTROLA SI LA SUMA DE LOS TICKETS SUPERA 5 UNIDADES
+    //RETORNA VALOR TOTAL DE TODOS LOS TICKETS 
+    //LA CUENTA ES LA SUMATORIA DE: 
+    //VALOR TICKET TOTAL * (1-DESCUENTO DE ESA CATEGORIA) * CANTIDAD DE TICKETS DE ESA CATEGORIA
+    //(TICKET_VALUE*(1-STUDENT_DISCOUNT)*ticketsStudents) + (TICKET_VALUE*(1-TRAINEE_DISCOUNT)*ticketsTrainees) + (TICKET_VALUE*(1-JUNIOR_DISCOUNT)*ticketsJuniors)
+
+    ticketsStudentsNumber = parseInt(ticketsStudents.value);
+    ticketsTraineesNumber = parseInt(ticketsTrainees.value);
+    ticketsJuniorsNumber = parseInt(ticketsJuniors.value);
+    ticketsGeneralsNumber = parseInt(ticketsGenerals.value);
+
+    let total
+
+    if (valid_total_ticket_number()) {
+        let studentsTotal = TICKET_VALUE * (1 - STUDENT_DISCOUNT) * ticketsStudentsNumber;
+        let traineesTotal = TICKET_VALUE * (1 - TRAINEE_DISCOUNT) * ticketsTraineesNumber;
+        let juniorsTotal = TICKET_VALUE * (1 - JUNIOR_DISCOUNT) * ticketsJuniorsNumber;
+        let generalsTotal = TICKET_VALUE * ticketsGeneralsNumber;
+        total = (studentsTotal + traineesTotal + juniorsTotal + generalsTotal).toFixed(2);
+
+        document.getElementById("error-message").innerHTML = "";
+        document.getElementById("error-message").hidden = true;
+
+    } else {
+        total = "-"
+
+        document.getElementById("error-message").innerHTML = "Error: La cantidad total de tickets no puede ser superior a 5";
+        document.getElementById("error-message").hidden = false;
+    }
+    document.getElementById("total-cost-tickets").innerHTML = "Total a pagar: $" + total;
+}
+
 function valid_total_ticket_number() {
     //VALIDA QUE LA CANTIDAD TOTAL DE TICKETS SEA VALIDA
     //DEVUELVE TRUE EN CASO DE QUE LA CANTIDAD TOTAL DE TICKETS SEA MENOR O IGUAL A CINCO
     //EN CASO CONTRARIO DEVUELVE FALSE
-    let ticketsStudents = parseInt(document.getElementById("tickets-students").value);
-    let ticketsTrainees = +document.getElementById("tickets-trainees").value;
-    let ticketsJuniors = +document.getElementById("tickets-juniors").value;
-    let ticketsGenerals = +document.getElementById("tickets-generals").value;
 
-    return (ticketsJuniors + ticketsStudents + ticketsTrainees + ticketsGenerals) <= 5
+    return (ticketsJuniorsNumber + ticketsStudentsNumber + ticketsTraineesNumber + ticketsGeneralsNumber) <= 5
 }
-
-
-
-
