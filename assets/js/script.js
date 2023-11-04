f2bd26079d3820dd9b867d910af0dd389b6c3a71
Braulio Boada <brathlowiis@gmail.com>
2023-11-03 23:50:56 -0300

    Finalizando Funcionalidad de TK

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,73 +18,21 @@ let ticketsGeneralsNumber = 0;
 
 // EVENTLISTENERS PARA OBSERVAR VALORES DE LOS SELECTORES DE TKTS AL CAMBIARLOS
 ticketsStudents.addEventListener("change", function () {
-    console.log("tkts students: " + ticketsStudents.value);
+    //console.log("tkts students: " + ticketsStudents.value);
     show_total_price();
 });
 ticketsTrainees.addEventListener("change", function () {
-    console.log("tkts trainees: " + ticketsTrainees.value);
+    //console.log("tkts trainees: " + ticketsTrainees.value);
     show_total_price();
 });
 ticketsJuniors.addEventListener("change", function () {
-    console.log("tkts juniors: " + ticketsJuniors.value);
+    //console.log("tkts juniors: " + ticketsJuniors.value);
     show_total_price();
 });
 ticketsGenerals.addEventListener("change", function () {
-    console.log("tkts generals: " + ticketsJuniors.value);
+    //console.log("tkts generals: " + ticketsJuniors.value);
     show_total_price();
 });
-
-function tickets_total_count() {
-    
-
-    //Numero de PV
-    const numramdom = [1596, 2586, 6574, 4586, 9658, 1452, 3652];
-    const randomIndex = Math.floor(Math.random() * numramdom.length);
-    const randomNumber = numramdom[randomIndex];
-    let aleatorioPV = document.createTextNode(randomNumber);
-    document.getElementById('numPV').appendChild(aleatorioPV);
-
-    // Nombre, Apellido y Email
-    let nombre = document.getElementById("input-name").value;
-    let apellido = document.getElementById("input-lastname").value;
-    let persona = nombre.toUpperCase() + " " + apellido.toUpperCase();
-    let e_mail = document.getElementById("input-email").value.toUpperCase();
-    let cliente = document.createTextNode(persona);
-    let correo = document.createTextNode(e_mail);
-    document.getElementById('comprador').appendChild(cliente);
-    document.getElementById('emailComprador').appendChild(correo);
-
-    //Fecha
-    const fechaOutput = document.getElementById("fechaOutput");
-    const fechaActual = new Date();
-    const formatoFecha = {
-        day: 'numeric',
-        month: 'numeric',
-        year: 'numeric',
-    };
-    const fechaFormateada = fechaActual.toLocaleDateString(undefined, formatoFecha);
-    fechaOutput.textContent = fechaFormateada;
-
-    //hora
-    const horaOutput = document.getElementById("horaOutput");
-    const horaActual = new Date();
-    const formatoHora = {
-        hour: '2-digit',
-        minute: '2-digit',
-    };
-    const horaFormateada = horaActual.toLocaleTimeString(undefined, formatoHora);
-    horaOutput.textContent = horaFormateada + "h";
-
-    //Numero de NR.T
-    let aleatorioNR_T = document.createTextNode(randomNumber);
-    document.getElementById('numNR').appendChild(aleatorioNR_T);
-
-    //Precio Unitario
-    let PrecioUnitario = document.getElementById("PrecioUnitario");
-    PrecioUnitario.textContent = "$" + TICKET_VALUE;
-
-    
-}
 
 function reset_form() {
     //RESETEA EL FORMULARIO
@@ -136,4 +84,99 @@ function valid_total_ticket_number() {
     //EN CASO CONTRARIO DEVUELVE FALSE
 
     return (ticketsJuniorsNumber + ticketsStudentsNumber + ticketsTraineesNumber + ticketsGeneralsNumber) <= 5
+}
+
+function tickets_total_count() {
+
+    //Numero de PV
+    const numramdom = [1596, 2586, 6574, 4586, 9658, 1452, 3652];
+    const randomIndex = Math.floor(Math.random() * numramdom.length);
+    const randomNumber = numramdom[randomIndex];
+    let aleatorioPV = document.createTextNode(randomNumber);
+    document.getElementById('numPV').appendChild(aleatorioPV);
+
+    // Nombre, Apellido y Email
+    let nombre = document.getElementById("input-name").value;
+    let apellido = document.getElementById("input-lastname").value;
+    let persona = nombre.toUpperCase() + " " + apellido.toUpperCase();
+    let e_mail = document.getElementById("input-email").value.toUpperCase();
+    let cliente = document.createTextNode(persona);
+    let correo = document.createTextNode(e_mail);
+    document.getElementById('comprador').appendChild(cliente);
+    document.getElementById('emailComprador').appendChild(correo);
+
+    //Fecha
+    const fechaOutput = document.getElementById("fechaOutput");
+    const fechaActual = new Date();
+    const formatoFecha = {
+        day: 'numeric',
+        month: 'numeric',
+        year: 'numeric',
+    };
+    const fechaFormateada = fechaActual.toLocaleDateString(undefined, formatoFecha);
+    fechaOutput.textContent = fechaFormateada;
+
+    //hora
+    const horaOutput = document.getElementById("horaOutput");
+    const horaActual = new Date();
+    const formatoHora = {
+        hour: '2-digit',
+        minute: '2-digit',
+    };
+    const horaFormateada = horaActual.toLocaleTimeString(undefined, formatoHora);
+    horaOutput.textContent = horaFormateada + "h";
+
+    //Numero de NR.T
+    let aleatorioNR_T = document.createTextNode(randomNumber);
+    document.getElementById('numNR').appendChild(aleatorioNR_T);
+
+    //Cantidad de Boletos
+    let cantidadBoletos = ticketsJuniorsNumber + ticketsStudentsNumber + ticketsTraineesNumber + ticketsGeneralsNumber;
+
+    let boletos = document.createTextNode("(0" + cantidadBoletos + ")");
+    document.getElementById('boletos').appendChild(boletos);
+
+    //Precio Unitario
+    let PrecioUnitario = document.getElementById("PrecioUnitario");
+    PrecioUnitario.textContent = "$" + TICKET_VALUE + ",00";
+
+    //Precio Total
+    let PrecioTotal = document.getElementById("PrecioTotal");
+    PrecioTotal.textContent = "($" + (TICKET_VALUE * cantidadBoletos) + ",00)";
+
+    //Descuento Estudiante
+    if (ticketsStudentsNumber > 0) {
+        let dec1 = document.getElementById("dec1");
+        dec1.textContent = "DESC. ESTUDIANTE";
+        let desStudent = document.getElementById("desStudent");
+        desStudent.textContent = "- $" + ((ticketsStudentsNumber * TICKET_VALUE) * STUDENT_DISCOUNT) + ",00";
+        document.getElementById("secStudent").style.display = "block";
+    }
+
+    //Descuento Trainee
+    if (ticketsTraineesNumber > 0) {
+        let dec2 = document.getElementById("dec2");
+        dec2.textContent = "DESC. TRAINEE";
+        let desTrainee = document.getElementById("desTrainee");
+        desTrainee.textContent = "- $" + ((ticketsTraineesNumber * TICKET_VALUE) * TRAINEE_DISCOUNT) + ",00";
+        document.getElementById("secTrainee").style.display = "block";
+    }
+
+    //Descuento Junior
+    if (ticketsJuniorsNumber > 0) {
+        let dec3 = document.getElementById("dec3");
+        dec3.textContent = "DESC. JUNIOR";
+        let desJunior = document.getElementById("desJunior");
+        desJunior.textContent = "- $" + ((ticketsJuniorsNumber * TICKET_VALUE) * JUNIOR_DISCOUNT) + ",00";
+        document.getElementById("secJunior").style.display = "block";
+    }
+
+    let studentsTotal = TICKET_VALUE * (1 - STUDENT_DISCOUNT) * ticketsStudentsNumber;
+    let traineesTotal = TICKET_VALUE * (1 - TRAINEE_DISCOUNT) * ticketsTraineesNumber;
+    let juniorsTotal = TICKET_VALUE * (1 - JUNIOR_DISCOUNT) * ticketsJuniorsNumber;
+    let generalsTotal = TICKET_VALUE * ticketsGeneralsNumber;
+    let montoTotal = (studentsTotal + traineesTotal + juniorsTotal + generalsTotal);
+
+    let totalPago = document.getElementById("totalPago");
+    totalPago.textContent = "$" + montoTotal + ",00";
 }
